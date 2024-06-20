a84a5f7e8d4da495d097cf8e3f47ff5a39e52555
Bhumit5 <bhumits190@gmail.com>
2024-06-20 13:51:08 +0530

    Backend of hablar box is ready with connection of atlas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const socket = require("socket.io");
-const PORT = 10000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
